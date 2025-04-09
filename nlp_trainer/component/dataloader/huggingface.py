3ac096236e855a817adcde77da798b008b79fddc
Wonbin Kim <kwb0711@gmail.com>
2025-04-10 03:08:36 +0900

    Update components

--- nlp_trainer/component/dataloader/huggingface.py
+++ nlp_trainer/component/dataloader/huggingface.py
@@ -1,4 +1,4 @@
-from nlp_trainer.core.domain.entity.dataloader import Dataloader
+from nlp_trainer.core.domain.port.dataloader import Dataloader
 from datasets import load_dataset
 from torch.utils import data
 from typing import Iterable, Any
@@ -16,9 +16,9 @@ class HuggingFaceDataLoader(Dataloader):
         batch_size: int,
         num_workers: int,
         pin_memory: bool,
-        prefetch_factor: int,
-        persistent_workers: bool,
-        timeout: int,
+        prefetch_factor: int = 2,
+        persistent_workers: bool = True,
+        timeout: int = 1000,
     ):
         dataset = load_dataset(path, name, split=split, streaming=streaming)
         self.dataloader = data.DataLoader(
@@ -40,8 +40,17 @@ class HuggingFaceDataLoader(Dataloader):
 
 if __name__ == "__main__":
     fw = HuggingFaceDataLoader(
-        "HuggingFaceFW/fineweb-edu", "CC-MAIN-2024-10", "train", streaming=True
-    )
+        "HuggingFaceFW/fineweb-edu",
+        "CC-MAIN-2024-10",
+        "train",
+        streaming=True,
+        batch_size=1,
+        num_workers=1,
+        pin_memory=True,
+        prefetch_factor=2,
+        persistent_workers=True,
+        timeout=1000,
+    ).get_batch_iterator()
+
     for i in fw:
-        print(i)
-        break
+        print(len(i["text"]))
--- nlp_trainer/component/loss/perplexity.py
+++ nlp_trainer/component/loss/perplexity.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-from nlp_trainer.core.port.loss import LossFunction
-from nlp_trainer.core.entity.loss import LossFunctionType
+from nlp_trainer.core.domain.port.loss import LossFunction
+from nlp_trainer.core.domain.entity.loss import LossFunctionType
 from typing import TypedDict
 
 
@@ -14,8 +14,21 @@ class PerplexityLoss(LossFunction[PerplexityLossInput], nn.Module):
     def __init__(self):
         super(PerplexityLoss, self).__init__()
 
-    def get_loss_function_type(self) -> LossFunctionType:
+        self.loss_fct = nn.CrossEntropyLoss()
+
+    def get_type(self) -> LossFunctionType:
         return LossFunctionType.PERPLEXITY
 
     def calculate_loss(self, batch: PerplexityLossInput) -> torch.Tensor:
-        pass
+        logits = batch["y_pred"]
+        labels = batch["y_true"]
+
+        shift_logits = logits[:, :-1, :].contiguous()
+        shift_labels = labels[:, 1:].contiguous()
+
+        loss = self.loss_fct(
+            shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)
+        )
+        perplexity = torch.exp(loss)
+
+        return perplexity
